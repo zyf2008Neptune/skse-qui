6c5612f9603e8fb935689ae7e1c6d10398b9080e
Neptune <zyfrtas2008@gmail.com>
2026-02-20 22:58:28 +0800

    update xmake ci build

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,11 @@
 set_xmakever("2.9.2")
 
+--submodule
+includes(CommonLib)
+
 -- project
 set_project("QUI")
-set_version("0.3.3.0")
+set_version("0.4.0.0")
 set_license("MIT")
 set_languages("c++20")
 set_optimize("faster")
@@ -28,13 +31,19 @@ set_toolchains("msvc")
 add_requires("fmt", "frozen", "toml11")
 add_requires("spdlog", { configs = { header_only = false } })
 
-package("alandtse-CommonLib-ng")
-    set_base("commonlibsse-ng")
-    set_urls("https://github.com/alandtse/CommonLibVR/tree/ng.git")
-package_end()
+-- targets
+target("QUI")
+-- add dependencies to target
+    add_deps("commonlibsse-ng")
 
-add_requires("alandtse-CommonLib-ng ng", {alias ="commonlibsse-ng", configs = { skyrim_vr = false , skyrim_se = false }})
-	
-includes("res/package.lua")
-includes("src")
+    -- add commonlibsse-ng plugin
+    add_rules("commonlibsse-ng.plugin", {
+        name = "QUI",
+        description = "SKSE64 plugin using CommonLibSSE-NG"
+    })
 
+    -- add src files
+    add_files("src/**.cpp" "src/**.cc")
+    add_headerfiles("src/**.h" "src/**.hpp")
+    add_includedirs("src")
+    set_pcxxheader("src/quipch.hpp")
